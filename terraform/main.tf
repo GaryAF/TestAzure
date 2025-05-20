9bc37223ff2a74536bf7b26297c532078f3b6623
Garyy <garyy@MacBook-Pro-de-Garyy.local>
2025-05-20 17:15:19 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "gary_test_good_${var.environment}"
   location = var.location
 }
+
+resource "azurerm_container_registry" "acr" {
+
+  name                = "garytestacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
